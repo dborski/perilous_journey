7b5ef522702a08fba344bcfe8d23188333a8ea2c
Derek Borski <derek.borski@gmail.com>
2020-07-28 09:36:19 -0600

    Append method and spec

--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -1,5 +1,6 @@
 require_relative 'test_helper'
 require './lib/linked_list'
+require './lib/node'
 
 class LinkedListTest < Minitest::Test
 
@@ -9,5 +10,16 @@ class LinkedListTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of LinkedList, @list
+  end
+
+  def test_node_head_is_nil_by_default
+    assert_nil @list.head
+  end
+
+  def test_append_list
+    @list.append("West")
+
+    assert_instance_of Node, @list.head
+    assert_nil @list.head.next_node
   end
 end 
