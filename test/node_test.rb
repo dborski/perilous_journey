5dd7038ca4b57acd52e92fd8752e43f443bb2813
Derek Borski <derek.borski@gmail.com>
2020-07-28 09:24:18 -0600

    Complete iteration 1

--- test/node_test.rb
+++ test/node_test.rb
@@ -14,4 +14,8 @@ class NodeTest < Minitest::Test
   def test_it_has_readable_attributes
     assert_equal "Burke", @node.surname
   end
+
+  def test_next_node_is_nil_by_default
+    assert_nil @node.next_node
+  end 
 end 
